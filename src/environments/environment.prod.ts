05e8a15789a0bb78705f4c785c04f14c0da42511
unknown <perez.artaso@gmail.com>
2021-09-09 21:04:27 -0300

    enviroment.prod.ts agregado código inseguro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMvdGHf882S09qvUJ6mM92GCVMVjRRbGY",
+    authDomain: "la-sala-eb971.firebaseapp.com",
+    projectId: "la-sala-eb971",
+    storageBucket: "la-sala-eb971.appspot.com",
+    messagingSenderId: "305602870419",
+    appId: "1:305602870419:web:857f21c216a91d13f22166",
+    measurementId: "G-QF7MY9G7R7"
+  }
 };
